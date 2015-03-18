d8bc0a75e0c10dfc34e86b624eb7663a2c1a78ec
Vaibhav Singh <techvaibhav@gmail.com>
2015-03-18 11:36:22 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 $( function() {
 // Add background image
 	$.backstretch('images/road2.jpg');
-	var endDate = "February 20, 2015 15:03:25";
+	var endDate = "April 1, 2015 15:03:25";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
 	  date: endDate,
